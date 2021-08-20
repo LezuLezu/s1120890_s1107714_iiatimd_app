0d4f25ec0ca1b641bcca53361565bee179d1d894
Dishang <dishang04@gmail.com>
2021-08-20 11:44:01 +0200

    made some final changes

--- app/src/main/java/com/iatjrd/geld_lenen_app/MainActivity.java
+++ app/src/main/java/com/iatjrd/geld_lenen_app/MainActivity.java
@@ -13,6 +13,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
